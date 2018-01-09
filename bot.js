740078acc0a953c85d2c00d51b590259948ab302
KinqRoot <35272385+KinqRoot@users.noreply.github.com>
2018-01-09 22:10:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,14 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!sa') {
-    	message.reply('Es wurden noch keine Termine eingetragen. Wenn du die Termine aber hast, sag @KinqRoot bescheid!!!');
+    	message.reply('Schularbeiten:
+                      -Mathe: 11.1.2018');
   	}
     if (message.content === '!test') {
-    	message.reply('Es wurden noch keine Termine eingetragen. Wenn du die Termine aber hast, sag @KinqRoot bescheid!!!');
+    	message.reply('Tests/Wiederholungen:
+                      -Deutsch: 10.1.2018
+                      -Netzwerktechnik: 15.1.2018
+                      -ITP2: 19.1.2018');
   	}
     if (message.content === '!termall') {
     	message.reply('Es wurden noch keine Termine eingetragen. Wenn du die Termine aber hast, sag @KinqRoot bescheid!!!');
