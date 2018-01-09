70fdedae465aa53c80b16b75f792de6bb9eb4099
KinqRoot <35272385+KinqRoot@users.noreply.github.com>
2018-01-09 22:48:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,19 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!sa') {
-    	message.reply('Schularbeiten: \n Mathe: 11.1.2018');
+    	message.reply('\n Schularbeiten: \n Mathe: 11.1.2018');
   	}
     if (message.content === '!test') {
-    	message.reply('Tests/Wiederholungen: \n Deutsch: 10.1.2018 \n Netzwerktechnik: 15.1.2018 \n ITP2: 19.1.2018');
+    	message.reply('\n Tests/Wiederholungen: \n Deutsch: 10.1.2018 \n Netzwerktechnik: 15.1.2018 \n ITP2: 19.1.2018');
   	}
     if (message.content === '!termall') {
-    	message.reply('Schularbeiten: \n Mathe: 11.1.2018 \n \n Tests/Wiederholungen: \n Deutsch: 10.1.2018 \n Netzwerktechnik: 15.1.2018 \n ITP2: 19.1.2018')
+    	message.reply('\n Schularbeiten: \n Mathe: 11.1.2018 \n \n Tests/Wiederholungen: \n Deutsch: 10.1.2018 \n Netzwerktechnik: 15.1.2018 \n ITP2: 19.1.2018')
                  
 
   	}
+    if (message.conten === '!termhelp'){
+        message.reply('\n Termin Befehle: \n !sa Zeigt die Schularbeiten an \n !test Zeigt die Tests an \n !termall Zeigt alle Termine an')
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
