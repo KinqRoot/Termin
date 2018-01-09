f2e467aac962bb8c7605a618cb11899001e78bc2
KinqRoot <35272385+KinqRoot@users.noreply.github.com>
2018-01-09 22:15:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ client.on('message', message => {
     	message.reply('Tests/Wiederholungen: || Deutsch: 10.1.2018 || Netzwerktechnik: 15.1.2018 || ITP2: 19.1.2018');
   	}
     if (message.content === '!termall') {
-    	message.reply('Es wurden noch keine Termine eingetragen. Wenn du die Termine aber hast, sag @KinqRoot bescheid!!!');
+    	message.reply('Schularbeiten: || Mathe: 11.1.2018');
+        message('Tests/Wiederholungen: || Deutsch: 10.1.2018 || Netzwerktechnik: 15.1.2018 || ITP2: 19.1.2018');
   	}
 });
 
